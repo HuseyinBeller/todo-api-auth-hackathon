de74d40b658370eb9abb87b1a57a8acd99065248
Hüseyin Beller <bellerhuseyin@gmail.com>
2021-04-29 12:21:48 +0200

    upgraded

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,7 +58,7 @@ const User = require('../models/User')
     const validationErrors = []
     if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
     if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
-    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
+    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Pagit init
   
     if (validationErrors.length) {
       req.flash('errors', validationErrors)
